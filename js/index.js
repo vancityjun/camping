96538b035a669815dd5fb22537dc74a838aa80aa
Jun Lee <jun@Juns-MacBook-Pro-2.local>
2021-02-18 22:13:05 -0800

    add services&destinations sections

--- js/index.js
+++ js/index.js
@@ -1,10 +1,30 @@
 const Card = {
-  template: `<div>this is a Card</div>`,
+  template: `
+    <div class="card">
+      <div class="img-container" :class="isItem ? 'thumbnail' : null">
+        <div v-if="isItem" class="like"></div>
+        <img :src="imgUrl" alt=""/>
+      </div>
+      <span v-if="isItem" class="price">\${{price}}/night</span>
+      <h4 class="title">
+        {{title}}
+      </h4>
+      <p class="description">
+        {{description}}
+      </p>
+      <a v-if="isItem" href="javascript:void(0)" class="btn fill">Read More</a>
+    </div>
+  `,
   props: {
     imgUrl: String,
     title: String,
     description: String,
-    isItem: Boolean,
+    price: Number,
+    like: Boolean,
+    isItem: {
+      type: Boolean,
+      default: false
+    },
   }
 }
 
@@ -12,5 +32,38 @@ const app = new Vue({
   el: '#app',
   components: {
     'card' : Card,
+  },
+  data: {
+    services: [
+      {
+        imgUrl: './assets/fire.svg',
+        title: 'Camping & Day Use', 
+        description: 'Return to your favorite spot or discover a new one that’s right for you.'
+      },
+      {
+        imgUrl: './assets/fire.svg',
+        title: 'Camping & Day Use', 
+        description: 'Return to your favorite spot or discover a new one that’s right for you.'
+      },
+      {
+        imgUrl: './assets/fire.svg',
+        title: 'Camping & Day Use', 
+        description: 'Return to your favorite spot or discover a new one that’s right for you.'
+      },
+      {
+        imgUrl: './assets/fire.svg',
+        title: 'Camping & Day Use', 
+        description: 'Return to your favorite spot or discover a new one that’s right for you.'
+      },
+    ],
+    destinations: [
+      {
+        imgUrl: './assets/image4.jpg',
+        title: 'Luxury Tiny Beach Cabin', 
+        description: 'Book unique camping experiences on over 300,000 campsites.',
+        price: 36,
+        like: false
+      },
+    ]
   }
 })
